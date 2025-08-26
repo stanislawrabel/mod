dca9b796d7e43ca97c6f2772fc60a9ffb4569821
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2025-08-26 06:17:51 +0200

    Update All_regions_m.sh

--- All_regions_m.sh
+++ All_regions_m.sh
@@ -95,7 +95,7 @@ run_ota() {
   clean_model=$(echo "$device_model" | sed 's/IN\|RU\|TR\|EEA\|T2//g')
   base_model="$clean_model"
 
-  ota_command="realme-ota $server $device_model ${base_model}_11.${version}.00_0000_000000000000 6 $nv_id"
+  ota_command="realme-ota $server $device_model ${base_model}_11.${version}.01_0001_100001010000 6 $nv_id"
   
 
   output=$(eval "$ota_command")
