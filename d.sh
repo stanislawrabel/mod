cf65e2a6fd52f8d29048788a5f12096a37c1ccb1
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2025-09-06 08:58:53 +0200

    Update d.sh

--- d.sh
+++ d.sh
@@ -20,10 +20,10 @@ if ! command -v aria2c &> /dev/null; then
 fi
 
 clear
-echo -e "${GREEN}+==============================================+${RESET}"
-echo -e "${GREEN}|===${RESET}      ${YELLOW}DownloadeR${RESET}   ${RED}by${RESET} ${BLUE}Stano 36${RESET}          ${GREEN}===|${RESET}"                          
-echo -e "${GREEN}|===${RESET} ${YELLOW_BG}${BLACK}   realme   ${RESET} ${GREEN_BG}${WHITE}    oppo   ${RESET} ${RED_BG}${WHITE}   OnePlus   ${RESET} ${GREEN}===|${RESET}"
-echo -e "${GREEN}+==============================================+${RESET}"
+echo -e "${GREEN}+========================================+${RESET}"
+echo -e "${GREEN}|${RESET}      ${YELLOW}DownloadeR${RESET}   ${RED}by${RESET} ${BLUE}Stano 36${RESET}          ${GREEN}===|${RESET}"                          
+echo -e "${GREEN}|${RESET} ${YELLOW_BG}${BLACK}   realme   ${RESET} ${GREEN_BG}${WHITE}    oppo   ${RESET} ${RED_BG}${WHITE}   OnePlus   ${RESET} ${GREEN}|${RESET}"
+echo -e "${GREEN}+========================================+${RESET}"
 echo -e
 read -p "Enter URL: " url
 
