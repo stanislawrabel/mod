d54f2940afff523a23b69e5c16bde6cf341b8852
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2025-09-06 09:56:31 +0200

    Update o.sh

--- o.sh
+++ o.sh
@@ -172,6 +172,7 @@ clear
 
 echo -e "${GREEN}+=====================================+${RESET}"
 echo -e "${GREEN}|==${RESET} ${GREEN}   OTA FindeR${RESET} ${RED}  by${RESET} ${BLUE}Stano36${RESET}   ${GREEN}   ==|${RESET}"
+echo -e "${GREEN}+=====================================+${RESET}"
 echo -e "${GREEN}|${RESET} ${YELLOW_BG}${BLACK}  realme   ${RESET} ${GREEN_BG}${BLACK}   oppo   ${RESET} ${RED_BG}${WHITE}  OnePlus   ${RESET} ${GREEN}|${RESET}"
 echo -e "${GREEN}+=====================================+${RESET}"
 printf "| %-5s | %-6s | %-18s |\n" "Mani." "R code" "Region"
